e2eef8160ed72e2204ef952675c08ec5c3a7bd5f
Vodnik48 <mr.Vodnik48@yandex.ru>
2023-02-08 17:53:22 +0300

    podgotovka__rev

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const popup = document.querySelector('.popup');
+const popupElement = document.querySelector('.popup');
 const closeButton = popupElement.querySelector(".popup__button-close");
 const editButton = document.querySelector('.profile__edit-button');
 const formElement = document.querySelector(".popup__form");
@@ -11,41 +11,41 @@ const openPopup = function () {
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
     popupElement.classList.add("popup_opened");
-    };
-
-popupEdit.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    popup.classList.add('.popup_opened')
-})
-popupClose.addEventListener('click',() => {
-    evt.preventDefault();
-    popup.classList.remove('.popup_opened')
-})  
-
+};
 editButton.addEventListener("click", openPopup);
 
 const closePopup = function () {
     popupElement.classList.remove("popup_opened");
-    };
-    
-    closeButton.addEventListener("click", closePopup);
-    
+};
 
+closeButton.addEventListener("click", closePopup);
 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    closePopup();
+}
 
+formElement.addEventListener("submit", formSubmitHandler);
 
-    function formSubmitHandler(evt) {
-        evt.preventDefault();
-        profileName.textContent = nameInput.value;
-        profileJob.textContent = jobInput.value;
-        closePopup();
-        }
-        
-        formElement.addEventListener("submit", formSubmitHandler);
 
 
 /*let formElement = document.querySelector('.popup__container');
 
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
-let buttonSave = document.querySelector('.popup__button');*/
+let buttonSave = document.querySelector('.popup__button');
+
+popupEdit.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    popup.classList.add('.popup_opened')
+})
+popupClose.addEventListener('click', () => {
+    evt.preventDefault();
+    popup.classList.remove('.popup_opened')
+})
+
+editButton.addEventListener("click", openPopup);
+
+*/
