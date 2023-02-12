452eec57e7737bec874fcf79f567f631ca245dc7
Vodnik48 <mr.Vodnik48@yandex.ru>
2023-02-12 17:14:35 +0300

    Podacha__PR4__7

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const popupElement = document.querySelector('.popup');
 const closeButton = popupElement.querySelector('.popup__button-close');
 const editButton = document.querySelector('.profile__edit-button');
 const formElement = document.querySelector('.popup__form');
-const nameInput = formElement.querySelector('.popup__profile-name');
-const jobInput = formElement.querySelector('.popup__profile-job');
+const nameInput = formElement.querySelector('.popup__input_profile-name');
+const jobInput = formElement.querySelector('.popup__input_profile-job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
@@ -31,21 +31,3 @@ formElement.addEventListener("submit", formSubmitHandler);
 
 
 
-/*let formElement = document.querySelector('.popup__container');
-
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_job');
-let buttonSave = document.querySelector('.popup__button');
-
-popupEdit.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    popup.classList.add('.popup_opened')
-})
-popupClose.addEventListener('click', () => {
-    evt.preventDefault();
-    popup.classList.remove('.popup_opened')
-})
-
-editButton.addEventListener("click", openPopup);
-
-*/
